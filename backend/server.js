f84ad236966a29a568598757036f129bef887cb7
syedzainulhadi <syedzainulhadi@gmail.com>
2026-05-09 00:56:56 +0530

    Fixed dynamic Vercel CORS issue

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) callback(null, true);
-    else callback(new Error("Not allowed by CORS"));
-  },
+  origin: true,
   credentials: true,
 }));
 
